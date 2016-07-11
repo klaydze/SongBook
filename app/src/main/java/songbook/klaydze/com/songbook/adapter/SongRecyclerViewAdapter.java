e82fb4d6ff46131b5bcc207a56bb1779a5f8791d
klaydze <jessiesemana@gmail.com>
2016-07-11 17:46:34 +0800

    Implement ClickListener in toggle button to add item as favorite

--- app/src/main/java/songbook/klaydze/com/songbook/adapter/SongRecyclerViewAdapter.java
+++ app/src/main/java/songbook/klaydze/com/songbook/adapter/SongRecyclerViewAdapter.java
@@ -6,8 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import io.realm.OrderedRealmCollection;
+import io.realm.Realm;
 import io.realm.RealmRecyclerViewAdapter;
 import songbook.klaydze.com.songbook.realm_model.SongModel;
 import songbook.klaydze.com.songbook.R;
@@ -16,6 +18,9 @@ import songbook.klaydze.com.songbook.R;
  * Created by Jessie on 7/8/2016.
  */
 public class SongRecyclerViewAdapter extends RealmRecyclerViewAdapter<SongModel, SongRecyclerViewAdapter.ViewHolder> {
+
+    Realm realm;
+
     public SongRecyclerViewAdapter(Context context, OrderedRealmCollection<SongModel> data) {
         super(context, data, true);
     }
@@ -28,15 +33,41 @@ public class SongRecyclerViewAdapter extends RealmRecyclerViewAdapter<SongModel,
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        SongModel song = getData().get(position);
+        realm = Realm.getDefaultInstance();
 
+        final SongModel song = getData().get(position);
+
+        final TextView itemSongTitle = (TextView) holder.itemView.findViewById(R.id.tvSongTitle);
+        final ToggleButton itemSongFavorite = (ToggleButton) holder.itemView.findViewById(R.id.toggleFavorite);
         TextView itemSongNumber = (TextView) holder.itemView.findViewById(R.id.tvSongNumber);
-        TextView itemSongTitle = (TextView) holder.itemView.findViewById(R.id.tvSongTitle);
         TextView itemSongArtist = (TextView) holder.itemView.findViewById(R.id.tvSongArtist);
 
         itemSongNumber.setText(song.getSongNumber());
         itemSongTitle.setText(song.getSongTitle());
         itemSongArtist.setText(song.getSongArtist());
+        itemSongFavorite.setChecked(song.isFavorite());
+
+        itemSongFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (itemSongFavorite.isChecked()) {
+                    realm.executeTransaction(new Realm.Transaction() {
+                        @Override
+                        public void execute(Realm realm) {
+                            song.setFavorite(itemSongFavorite.isChecked());
+                        }
+                    });
+                }
+                else {
+                    realm.executeTransaction(new Realm.Transaction() {
+                        @Override
+                        public void execute(Realm realm) {
+                            song.setFavorite(itemSongFavorite.isChecked());
+                        }
+                    });
+                }
+            }
+        });
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
